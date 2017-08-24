f8c79d28c3c01f03ec5dba4901720a194392f821
juniormbhele <juniormbhele@users.noreply.github.com>
2017-08-24 18:58:46 +0200

    Changing the properties file to be on the work space I'm currently using

--- ClientZone Automated Scripts/src/clientZone/ManualTransactions.java
+++ ClientZone Automated Scripts/src/clientZone/ManualTransactions.java
@@ -1,15 +1,9 @@
 package clientZone;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
-import org.testng.xml.XmlClass;
-import org.testng.xml.XmlSuite;
-import org.testng.xml.XmlTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManualTransactions 
 {
